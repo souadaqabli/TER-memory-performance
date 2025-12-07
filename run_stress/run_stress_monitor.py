7d8fc5fbae8be52682036c6fee12d6c37d173af6
souadaqabli <souadaqabli20@gmail.com>
2025-12-07 21:23:32 +0100

    rename-files

--- run_stress/run_stress_monitor.py
+++ run_stress/run_stress_monitor.py
@@ -4,7 +4,7 @@ import time
 
 # 1. Lancer le monitor en arrière-plan
 monitor_proc = subprocess.Popen(
-    ["python3", "monitor.py", "--out", "../results/run_monitor.csv", "--interval", "1"]
+    ["python3", "monitor.py", "--out", "../results/run_monitor_copy.csv", "--interval", "1"]
 )
 
 # 2. Lancer le stress mémoire
--- run_stress/run_stress_monitor_copy.py
+++ run_stress/run_stress_monitor_copy.py
@@ -4,12 +4,12 @@ import time
 
 # 1. Lancer le monitor en arrière-plan
 monitor_proc = subprocess.Popen(
-    ["python3", "monitor.py", "--out", "../results/run_monitor_rand.csv", "--interval", "1"]
+    ["python3", "monitor.py", "--out", "../results/run_monitor.csv", "--interval", "1"]
 )
 
 # 2. Lancer le stress mémoire
 stress_proc = subprocess.run(
-    ["python3", "mem_stress.py", "--mode", "rand", "--size-mb", "1024", "--iters", "50"]
+    ["python3", "mem_stress.py", "--mode", "copy", "--size-mb", "1024", "--iters", "50"]
 )
 
 # 3. Arrêter le monitor quand le stress est fini
