123039177309158bcf248594f57a9169d71fe08d
souadaqabli <souadaqabli20@gmail.com>
2026-02-03 15:29:45 +0100

    nouveau essai

--- run_stress/analyse3.py
+++ run_stress/analyse3.py
@@ -1,101 +1,99 @@
 import matplotlib.pyplot as plt
-import mem_stress 
-import numpy as np  
+import mem_stress2
+import numpy as np
 import os
 
-
 def run_comparison():
-    print("=== Analyse Complète : 4 Modes x  Tailles ===")
+    print("=== Analyse Min/Max : Benchmark en KiloOctets (Ko) ===")
     
-    # Vos tailles cibles
-    target_sizes = [2, 8, 64, 128, 512, 1024, 2048]
+    # LISTE EN KILO-OCTETS (Ko)
+    # 32 Ko = L1 typique
+    # 256 Ko - 1024 Ko = L2 typique
+    # 1024 Ko (1 Mo) et plus = L3 puis RAM
+    target_sizes_kb = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 65536]
+    
     output_dir = "../results"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    # Structure : 'mode': {'mean': [], 'std': []}
     data = {
-        'Seq Read':   {'x': [], 'y': [], 'yerr': [], 'c': '#1f77b4', 'fmt': 'o'}, # Bleu
-        'Seq Write':  {'x': [], 'y': [], 'yerr': [], 'c': '#d62728', 'fmt': 'o'}, # Rouge
-        'Rand Read':  {'x': [], 'y': [], 'yerr': [], 'c': '#ff7f0e', 'fmt': 'o'}, # Orange
-        'Rand Write': {'x': [], 'y': [], 'yerr': [], 'c': '#2ca02c', 'fmt': 'o'}  # Vert
+        'Seq Read':   {'x': [], 'y': [], 'y_err_low': [], 'y_err_high': [], 'c': '#1f77b4'},
+        'Rand Read':  {'x': [], 'y': [], 'y_err_low': [], 'y_err_high': [], 'c': '#ff7f0e'},
+        'Rand Write': {'x': [], 'y': [], 'y_err_low': [], 'y_err_high': [], 'c': '#2ca02c'}
     }
     
-    # Paramètres de test
-    ITERS_SEQ = 50       
-    DURATION_RAND = 5   
-    BATCH_SIZE = 50000   # Taille standard du batch
+    ITERS_SEQ = 100      # Un peu plus d'itérations pour les petites tailles
+    DURATION_RAND = 10 # Rapide
+    BATCH_SIZE = 50000 
 
-    for size_mb in target_sizes:
-        print(f"--- Test en cours pour taille : {size_mb} MB ---")
+    for size_kb in target_sizes_kb:
+        # Affichage pour suivre
+        if size_kb < 1024:
+            print(f"--- Benchmarking : {size_kb} Ko ---")
+        else:
+            print(f"--- Benchmarking : {size_kb/1024:.1f} Mo ---")
         
-        # 1. SEQUENTIAL READ (Bleu)
-        # Rappel return: gb_s, time, AVG_LAT, list_lat
-        _, _, lat, lst = mem_stress.sequential_read(size_mb, ITERS_SEQ)
-        data['Seq Read']['x'].append(size_mb)
-        data['Seq Read']['y'].append(lat)
-        data['Seq Read']['yerr'].append(np.std(lst))
+        # --- CONVERSION : Kilo -> Bytes ---
+        real_size_bytes = int(size_kb * 1024)
         
-        # 2. SEQUENTIAL WRITE ()
-        # Rappel return: gb_s, time, AVG_LAT, list_lat
-        _, _, lat, lst = mem_stress.sequential_write(size_mb, ITERS_SEQ)
-        data['Seq Write']['x'].append(size_mb)
-        data['Seq Write']['y'].append(lat)
-        data['Seq Write']['yerr'].append(np.std(lst))
+        # 1. SEQUENTIAL READ
+        _, _, avg, mini, maxi = mem_stress2.sequential_read(real_size_bytes, ITERS_SEQ)
+        
+        data['Seq Read']['x'].append(size_kb) # Axe X en Ko
+        data['Seq Read']['y'].append(avg)
+        data['Seq Read']['y_err_low'].append(avg - mini) 
+        data['Seq Read']['y_err_high'].append(maxi - avg)
+        
+        # 2. RANDOM READ
+        _, avg, mini, maxi = mem_stress2.random_access_test(real_size_bytes, DURATION_RAND, batch=BATCH_SIZE)
+        
+        data['Rand Read']['x'].append(size_kb)
+        data['Rand Read']['y'].append(avg)
+        data['Rand Read']['y_err_low'].append(avg - mini)
+        data['Rand Read']['y_err_high'].append(maxi - avg)
 
+        # 3. RANDOM WRITE
+        _, avg, mini, maxi = mem_stress2.random_write_test(real_size_bytes, DURATION_RAND, batch=BATCH_SIZE)
+        
+        data['Rand Write']['x'].append(size_kb)
+        data['Rand Write']['y'].append(avg)
+        data['Rand Write']['y_err_low'].append(avg - mini)
+        data['Rand Write']['y_err_high'].append(maxi - avg)
 
-        # 2. RANDOM READ (Orange)
-        # Rappel return: ops, AVG_LAT, list_lat
-
-        _, lat, lst = mem_stress.random_access_test(size_mb, DURATION_RAND, batch=BATCH_SIZE)
-        data['Rand Read']['x'].append(size_mb)
-        data['Rand Read']['y'].append(lat)
-        data['Rand Read']['yerr'].append(np.std(lst))
-
-        # 3. RANDOM WRITE (Vert)
-        # Rappel return: ops, AVG_LAT, list_lat
-        _, lat, lst = mem_stress.random_write_test(size_mb, DURATION_RAND, batch=BATCH_SIZE)
-        data['Rand Write']['x'].append(size_mb)
-        data['Rand Write']['y'].append(lat)
-        data['Rand Write']['yerr'].append(np.std(lst))
-
-    # --- Tracé du Graphique ---
-    plt.figure(figsize=(10, 7))
+    # --- Tracé ---
+    print("\n[INFO] Génération du graphique...")
+    plt.figure(figsize=(12, 8))
     
     for label, d in data.items():
-        # plt.errorbar est LA fonction pour les errors bars
-        # x, y : coordonnées
-        # yerr : taille des barres d'erreur (Standard Deviation)
-        # capsize : petite barre horizontale au bout de l'erreur
+        asymmetric_error = [d['y_err_low'], d['y_err_high']]
+        
         plt.errorbar(
-            d['x'], d['y'], yerr=d['yerr'], 
-            label=label, color=d['c'], fmt=d['fmt'], 
-            linewidth=2, markersize=6, capsize=4, alpha=0.9
+            d['x'], d['y'], 
+            yerr=asymmetric_error, 
+            label=label, color=d['c'], fmt='o', 
+            linewidth=2, markersize=5, capsize=4, alpha=0.9, ecolor=d['c']
         )
 
-    # --- Mise en forme ---
     plt.xscale('log')
-    #plt.yscale('log')
+    plt.yscale('log')
 
+    # On force l'affichage des ticks pour toutes nos tailles
     plt.xticks(
-        ticks=target_sizes, 
-        labels=[str(s) for s in target_sizes], 
+        ticks=target_sizes_kb, 
+        labels=[str(s) for s in target_sizes_kb], 
+        rotation=45
     )
 
-    # Axes et Titres
-    plt.xlabel('Taille du Bloc Mémoire (Mo)', fontsize=12, fontweight='bold')
-    plt.ylabel('Latence par accès (ns)', fontsize=12, fontweight='bold')
-    plt.title('Hiérarchie Mémoire : Évolution de la Latence & Stabilité', fontsize=14)
+    plt.xlabel('Taille du Bloc Mémoire (Ko)', fontsize=12, fontweight='bold')
+    plt.ylabel('Latence moyenne par element(ns)', fontsize=12, fontweight='bold')
+    plt.title('Performance Mémoire : L1 -> L2 -> RAM (Échelle Ko)', fontsize=14)
 
-    # Grille fine pour lecture log
     plt.grid(True, which="major", ls="-", alpha=0.6)
-    plt.grid(True, which="minor", ls=":", alpha=0.3)
+    plt.legend(fontsize=11, loc='upper left')
 
-    # Légende
-    plt.legend(fontsize=11, loc='upper left', frameon=True, shadow=True)
-
-
-    save_path = os.path.join(output_dir, "comparaison_std2.png")
+    save_path = os.path.join(output_dir, "comparaison_min_max.png")
     plt.savefig(save_path)
-    print(f"\n[OK] Graphique généré : {save_path}")
+    print(f"[OK] Graphique sauvegardé : {save_path}")
     plt.show()
 
 if __name__ == "__main__":
---
+++ run_stress/mem_stress2.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+# mem_stress.py -- various memory load patterns
+import numpy as np
+import time
+import argparse
+import multiprocessing as mp
+
+
+#-------------------------------------------------
+# 1. Sequential read  
+#-------------------------------------------------
+def sequential_read(size_bytes, iterations):
+    """
+    Benchmarks sequential memory read performance (linear access).
+    """
+    size = size_bytes // 8  # éléments float64 (8 bytes)
+    src = np.ones(size, dtype=np.uint64)
+    
+    stat = np.zeros(3, dtype = np.uint64)
+    stat[0] = np.iinfo(np.uint64).max
+    
+    t_start = time.perf_counter()
+    for _ in range(iterations):
+        t0 = time.perf_counter_ns()
+        _ = src.sum()  
+        t1 = time.perf_counter_ns()
+        
+        lat = t1 - t0
+        stat[0] = min(stat[0], lat)   # le minimum sur toutes les iterations
+        stat[1] = max(stat[1], lat)   # le maximum sur toutes les iterations
+        stat[2] = stat[2] + lat       # temps total passe a lire le tableau
+
+    t_end = time.perf_counter()
+    
+    bytes_processed = size_bytes * iterations
+    gb_s = bytes_processed / (t_end - t_start) / (1024**3)
+
+    avg_lat_iter = (stat[2] / iterations)   # latence moyenne par iteration, le temps moyen qu'il faut pour lire une fois tout le bloc mémoire
+    avg_lat_ns = (stat[2] / iterations) / len(src)          # latence moyenne par element 
+    min_ns = stat[0] / len(src) # le temps pour acceder a un seul element dans la meilleure iteration
+    max_ns = stat[1] / len(src)
+
+    return gb_s, t_end - t_start , avg_lat_ns, min_ns, max_ns
+
+
+# -------------------------------------------------------------------
+# 3-. SEQUENTIAL WRITE 
+# -------------------------------------------------------------------
+def sequential_write(size_bytes, iterations):
+    """
+    Benchmarks sequential memory write performance (linear fill).
+    """
+    size = size_bytes // 8 # float64
+    arr = np.ones(size, dtype=np.uint64)
+    
+    stat = np.zeros(3, dtype = np.uint64)
+    stat[0] = np.iinfo(np.uint64).max
+    
+    t_start = time.perf_counter()
+    
+    for _ in range(iterations):
+        t0 = time.perf_counter_ns()
+        arr[:] = 1 
+        t1 = time.perf_counter_ns()
+        
+        lat = t1 - t0
+        stat[0] = min(stat[0], lat)   # le minimum sur toutes les iterations
+        stat[1] = max(stat[1], lat)   # le maximum sur toutes les iterations
+        stat[2] = stat[2] + lat       # temps total passe a ecrire le tableau
+
+    t_end = time.perf_counter()
+    
+    bytes_processed = size_bytes * iterations
+    gb_s = bytes_processed / (t_end - t_start) / (1024**3)
+    
+    # Conversions
+    avg_lat_ns = (stat[2] / iterations) / len(arr) # latence moyenne par element 
+    min_ns = stat[0] / len(arr) # le temps pour ecrire un seul element dans la meilleure iteration
+    max_ns = stat[1] / len(arr)
+    
+    return gb_s, t_end - t_start, avg_lat_ns, min_ns, max_ns
+
+
+# -------------------------------------------------------------------
+# 4. RANDOM read (random access + latency)
+# -------------------------------------------------------------------
+def random_access_test(size_bytes, duration_s, batch=50000):
+    """
+    Measures complex random read access operations and average latency.
+    """
+    np.random.seed(0)
+    size = size_bytes // 8
+    # Optimisation: on lit des 1 (ne change rien à la latence, mais init + rapide)
+    arr = np.ones(size, dtype=np.uint64)
+    
+    stat = np.zeros(3, dtype=np.uint64)
+    stat[0] = np.iinfo(np.uint64).max
+    
+    start = time.time()
+    ops = 0
+    count_batches = 0
+
+    while time.time() - start < duration_s:
+        idx = np.random.randint(0, size, batch)
+        
+        t0 = time.perf_counter_ns()
+        _ = arr[idx].sum()
+        t1 = time.perf_counter_ns()
+        
+        lat = t1 - t0 # Temps pour traiter UN BATCH
+        
+        stat[0] = min(stat[0], lat)   # le minimum pour un batch
+        stat[1] = max(stat[1], lat)   # le maximum pour un batch
+        stat[2] = stat[2] + lat       # temps total cumulé
+        
+        ops += batch
+        count_batches += 1
+
+    # Conversions : Ici on divise par 'batch' car lat correspond à un lot de 'batch' accès
+    avg_lat_ns = (stat[2] / count_batches) / batch # latence moyenne par element
+    min_ns = stat[0] / batch # le temps pour un element dans le meilleur batch
+    max_ns = stat[1] / batch
+    
+    return ops / duration_s, avg_lat_ns, min_ns, max_ns
+
+# -------------------------------------------------------------------
+# 5. RANDOM WRITE (Aggressive Random Writes)
+# -------------------------------------------------------------------
+def random_write_test(size_bytes, duration_s, batch=50000):
+    """
+    Measures the performance of aggressive random memory writes.
+    """
+    np.random.seed(0)
+    size = size_bytes  // 8
+    arr = np.ones(size, dtype=np.uint64)
+    
+    stat = np.zeros(3, dtype=np.uint64)
+    stat[0] = np.iinfo(np.uint64).max
+    
+    start = time.time()
+    ops = 0
+    count_batches = 0
+
+    while time.time() - start < duration_s:
+        idx = np.random.randint(0, size, batch)
+        #vals = np.random.rand(batch) # On écrit quand même du random pour le réalisme
+        vals = np.random.randint(0, 100, batch, dtype=np.uint64)
+
+        t0 = time.perf_counter_ns()
+        arr[idx] = vals
+        t1 = time.perf_counter_ns()
+        
+        lat = t1 - t0
+        stat[0] = min(stat[0], lat)   # le minimum pour un batch
+        stat[1] = max(stat[1], lat)   # le maximum pour un batch
+        stat[2] = stat[2] + lat       # temps total cumulé
+        
+        ops += batch
+        count_batches += 1
+
+    # Conversions
+    avg_lat_ns = (stat[2] / count_batches) / batch # latence moyenne par element
+    min_ns = stat[0] / batch # le temps pour un element dans le meilleur batch
+    max_ns = stat[1] / batch
+    
+    return ops / duration_s , avg_lat_ns, min_ns, max_ns
+    
+
+def stride_test(size_bytes, duration_s, stride_bytes=4096):
+    # Test TLB (Saut variable)
+    stride_idx = stride_bytes // 8
+    if stride_idx < 1: stride_idx = 1
+    
+    size = size_bytes // 8
+    arr = np.random.rand(size)
+    
+    start = time.time()
+    ops = 0
+    while time.time() - start < duration_s:
+        # Lecture linéaire avec sauts
+        _ = arr[::stride_idx].sum() 
+        ops += (size // stride_idx)
+    return ops / duration_s
+
+
+# -------------------------------------------------------------------
+# MAIN
+# -------------------------------------------------------------------
+if __name__ == "__main__":
+    # GESTION UNIQUE DU SEED ICI (Global pour toutes les fonctions)
+    np.random.seed(0)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode",
+                        choices=["sequential_read", "sequential_write", "random_read", "random_write", "stride"],
+                        default="sequential_read")
+    parser.add_argument("--size-bytes", type=int, default=1024*1024*1024) # 1 Go par défaut
+    parser.add_argument("--iters", type=int, default=10)
+    parser.add_argument("--duration", type=int, default=10)
+    parser.add_argument("--procs", type=int, default=1)
+    parser.add_argument("--batch", type=int, default=50000)
+    parser.add_argument("--stride-bytes", type=int, default=4096)
+    args = parser.parse_args()
+
+    # NOTE : Mise à jour des prints pour afficher Avg (Moyenne), Min et Max.
+
+    #if args.mode == "copy":
+        #bw, dur, avg, mini, maxi = copy_test(args.size_bytes, args.iters)
+        #print(f"Copy {args.size_bytes} MiB | BW: {bw:.2f} GB/s | Lat: {avg:.2f} ns (Min: {mini:.2f}, Max: {maxi:.2f})")
+
+    if args.mode == "sequential_read":
+        bw, dur, avg, mini, maxi = sequential_read(args.size_bytes, args.iters)
+        print(f"Seq Read {args.size_bytes} MiB | BW: {bw:.2f} GB/s | Lat: {avg:.2f} ns (Min: {mini:.2f}, Max: {maxi:.2f})")
+
+    if args.mode == "sequential_write":
+        bw, dur, avg, mini, maxi = sequential_write(args.size_bytes, args.iters)
+        print(f"Seq Write {args.size_bytes} MiB | BW: {bw:.2f} GB/s | Lat: {avg:.2f} ns (Min: {mini:.2f}, Max: {maxi:.2f})")
+
+    elif args.mode == "random_read":
+        ops_s, avg, mini, maxi = random_access_test(args.size_bytes, args.duration, args.batch)
+        print(f"Rand Read {args.size_bytes} MiB | IOPS: {ops_s:.0f} | Lat: {avg:.2f} ns (Min: {mini:.2f}, Max: {maxi:.2f})")
+
+    elif args.mode == "random_write":
+        ops_s, avg, mini, maxi = random_write_test(args.size_bytes, args.duration, args.batch)
+        print(f"Rand Write {args.size_bytes} MiB | IOPS: {ops_s:.0f} | Lat: {avg:.2f} ns (Min: {mini:.2f}, Max: {maxi:.2f})")
+    
+    elif args.mode == "stride":
+        ops_s = stride_test(args.size_bytes, args.duration, args.stride_bytes)
+        print(f"Stride ops/s: {ops_s:.0f}")
